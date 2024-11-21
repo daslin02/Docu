7a12f4fd6bdf0ec4a325c908b486b69b803e8c8a
daslinNout <artemserbakov26@gmail.com>
2024-11-21 10:40:32 +0000

    mini update

--- script/genJson.py
+++ script/genJson.py
@@ -6,11 +6,14 @@ script , flag = argv[0], argv[1]
 array = []
 i = 2
 
+
 while i < len(argv):
     if (i+3 > len(argv)):
         break
     name , count ,unit , suplier = argv[i+0] , argv[i+1] , argv[i+2] , argv[i+3]        
     array.append((name,count,unit,suplier))
     i+=5
-js = json.dumps(array)
+with open("save.json" , "w") as file :
+    json.dump(array , file)
+
 print(array)
