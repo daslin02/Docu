ad7072ee989ebcc050f3eb045c7908657b03d1c4
daslin02 <artemserbakov26@gmail.com>
2025-01-07 20:56:01 +0000

    create win-convertor

--- move.cmd
+++ move.cmd
@@ -1,6 +1,6 @@
 @echo off
 IF EXIST "script/win-convertor" (
-    xcopy script/win-convertor build\
+    xcopy "script/win-convertor" "build\win-convertor\" /E /I
     echo win-convertor moved in build.
 ) ELSE (
     echo win-convertor no found.
